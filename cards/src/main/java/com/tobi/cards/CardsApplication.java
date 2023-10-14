5a6e7c9363653062fab47ac7bae48b4e719e3cc5
Beloved239 <adettob@gmail.com>
2023-10-15 00:03:40 +0100

    Initializing Loan Service

--- cards/src/main/java/com/tobi/cards/CardsApplication.java
+++ cards/src/main/java/com/tobi/cards/CardsApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CardsApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(CardsApplication.class, args);
 	}
